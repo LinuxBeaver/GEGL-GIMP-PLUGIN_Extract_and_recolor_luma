12e1f9d8f893157ba6ba5eef87496e00152a559d
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-07-15 22:17:05 +0000

    Add files via upload
    All updates happening June 15 2023 are to fix a crash in 2.99.16. No changes have been made.

--- colorizeluminance.c
+++ colorizeluminance.c
@@ -17,9 +17,7 @@
  * Colorize Luminance 2022 Beaver 
  */
 
-/*June 2023 recreation of GEGL Graph.
-If you paste this syntax in Gimp's GEGL graph you can test
-this filter without installing it.
+/*June 2023 recreation of GEGL Graph. If you paste this syntax in Gimp's GEGL graph you can test this filter without installing it.
 
 shadows-highlights
 levels 
@@ -33,22 +31,23 @@ gaussian-blur std-dev-x=0.5 std-dev-y=0.5 */
 
 #ifdef GEGL_PROPERTIES
 
-property_double (shadow, _("Exposure of highlights"), 0.0)
+property_double (shadow, _("Adjust exposure of highlights"), 0.0)
     description (_("Adjust exposure of highlights"))
     value_range (-100.0, 0.0)
 
 
 property_double (strength, _("Glow Strength"), 55.0)
-    description (_("Glow strength"))
-    value_range (0.0, G_MAXDOUBLE)
-    ui_range    (0.0, 100.0)
+    description (_("Bloom's Glow strength"))
+    value_range (0.0, 1000)
+    ui_range    (0.0, 300.0)
 
-property_double (levels, _("Opacity of the light"), 1.0)
-    description (_("The lower values the more intense"))
-    ui_range    (0.6, 2.0)
+property_double (levels, _("Control light"), 0.5)
+    description (_("Higher values make the light dampen, lower values make the light more intense."))
+    value_range    (0.1, 1.0)
+    ui_range    (0.1, 1.0)
 
 property_double (gaussian, _("Gaussian blur for Glow Effect"), 6.0)
-   description (_("Blur that applies to the whole filter"))
+   description (_("Blur that applies to the whole filter. Try it with the union composite mode on a Gimp layer above."))
    value_range (0.0, 95.0)
    ui_range    (0.24, 95.0)
    ui_gamma    (3.0)
@@ -95,28 +94,19 @@ static void attach (GeglOperation *operation)
                                   NULL);
 
    shadow = gegl_node_new_child (gegl,
-                                  "operation", "gegl:shadows-highlights",
+                                  "operation", "gegl:shadows-highlights", "highlights", 0.0,
                                   NULL);
 
 
-
-
-
   gegl_node_link_many (input, shadow, levels, bloom, lumen, color, gaussian, output, NULL);
-  gegl_node_connect_from (lumen, "aux", color, "output");
 
 
-
-
+  gegl_operation_meta_redirect (operation, "shadow", shadow, "highlights");
   gegl_operation_meta_redirect (operation, "strength", bloom, "strength");
   gegl_operation_meta_redirect (operation, "value", color, "value");
   gegl_operation_meta_redirect (operation, "gaussian", gaussian, "std-dev-x");
   gegl_operation_meta_redirect (operation, "gaussian", gaussian, "std-dev-y");
   gegl_operation_meta_redirect (operation, "levels", levels, "in-high");
-  gegl_operation_meta_redirect (operation, "shadow", shadow, "highlights");
-
-
-
 }
 
 static void
@@ -131,12 +121,10 @@ gegl_op_class_init (GeglOpClass *klass)
   gegl_operation_class_set_keys (operation_class,
     "name",               "gegl:colorizeluminance",
     "title",              _("Extract and Recolor Luminance"),
-    "categories",         "render",
-    "position-dependent", "true",
+    "categories",         "Lighting",
     "reference-hash",     "ffhaaa1kfk11ffacxx3gb10b073c9",
-    "reference-hashB",    "b1fbcf3h009155h2a00640b0a0a8791",
     "license",            "GPL3+",
-    "description", _("It is recommended that you use the Addition blend mode with mid opacity. If the color is white the Luminance and LCH Lightness blend mode will make it glow well. Extract and Colorize Luminance channel'"),
+    "description", _("GEGL Extracts the brightest pixels, recolors and blurs  them. This filter is meant to be fused with Gimp's blend modes, such as Addition mid opacity. If the color is white the Luminance and LCH Lightness blend mode will make it glow. Experiment with various blend modes and see what happens."),
     NULL);
 }
 
